a788211bfd4d373eda82a800ce99622201edb859
artium_96 <artium20@gmail.com>
2022-09-16 16:41:59 +0300

    change

--- index.js
+++ index.js
@@ -193,52 +193,52 @@ function checkBMI() {
 
 
 // all opertion calc
-function calcmn() {
+// function calcmn() {
 
-    let n1 = parseFloat(document.getElementById('n1').value);
-    let n2 = parseFloat(document.getElementById('n2').value);
+//     let n1 = parseFloat(document.getElementById('n1').value);
+//     let n2 = parseFloat(document.getElementById('n2').value);
 
-    let oper = document.getElementById('operators').value;
+//     let oper = document.getElementById('operators').value;
 
 
 
-    if (oper === '+') {
-        document.getElementById('resultCalcMini').innerHTML = n1 + n2;
-    }
+//     if (oper === '+') {
+//         document.getElementById('resultCalcMini').innerHTML = n1 + n2;
+//     }
 
-    else if (oper === '-') {
-        document.getElementById('resultCalcMini').innerHTML = n1 - n2;
-    }
+//     else if (oper === '-') {
+//         document.getElementById('resultCalcMini').innerHTML = n1 - n2;
+//     }
 
-    else if (oper === '/') {
-        document.getElementById('resultCalcMini').innerHTML = n1 / n2;
-    }
+//     else if (oper === '/') {
+//         document.getElementById('resultCalcMini').innerHTML = n1 / n2;
+//     }
 
-    else if (oper === '*') {
-        document.getElementById('resultCalcMini').innerHTML = n1 * n2;
-    }
+//     else if (oper === '*') {
+//         document.getElementById('resultCalcMini').innerHTML = n1 * n2;
+//     }
 
-}
+// }
 
 
 
 // check if value was number , we create input type text because we input number parseFloat
-function checkInputValue(element) {
+// function checkInputValue(element) {
 
 
-    let Boolres = isNaN(element.value);
+//     let Boolres = isNaN(element.value);
 
-    if (Boolres == false) {
+//     if (Boolres == false) {
 
-        element.style.border = '2px solid #989e92'
-    }
+//         element.style.border = '2px solid #989e92'
+//     }
 
-    if (Boolres == true) {
+//     if (Boolres == true) {
 
-        element.style.border = '2px solid red'
-        // document.getElementById('resultCalcMini').innerHTML = "Please input";
-    }
-}
+//         element.style.border = '2px solid red'
+//         // document.getElementById('resultCalcMini').innerHTML = "Please input";
+//     }
+// }
 
 
 
@@ -293,3 +293,192 @@ function exit() {
 
     window.location.reload(false);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+let countries = {
+    "am-ET": "Amharic",
+    "ar-SA": "Arabic",
+    "be-BY": "Bielarus",
+    "bem-ZM": "Bemba",
+    "bi-VU": "Bislama",
+    "bjs-BB": "Bajan",
+    "bn-IN": "Bengali",
+    "bo-CN": "Tibetan",
+    "br-FR": "Breton",
+    "bs-BA": "Bosnian",
+    "ca-ES": "Catalan",
+    "cop-EG": "Coptic",
+    "cs-CZ": "Czech",
+    "cy-GB": "Welsh",
+    "da-DK": "Danish",
+    "dz-BT": "Dzongkha",
+    "de-DE": "German",
+    "dv-MV": "Maldivian",
+    "el-GR": "Greek",
+    "en-GB": "English",
+    "es-ES": "Spanish",
+    "et-EE": "Estonian",
+    "eu-ES": "Basque",
+    "fa-IR": "Persian",
+    "fi-FI": "Finnish",
+    "fn-FNG": "Fanagalo",
+    "fo-FO": "Faroese",
+    "fr-FR": "French",
+    "gl-ES": "Galician",
+    "gu-IN": "Gujarati",
+    "ha-NE": "Hausa",
+    "he-IL": "Hebrew",
+    "hi-IN": "Hindi",
+    "hr-HR": "Croatian",
+    "hu-HU": "Hungarian",
+    "id-ID": "Indonesian",
+    "is-IS": "Icelandic",
+    "it-IT": "Italian",
+    "ja-JP": "Japanese",
+    "kk-KZ": "Kazakh",
+    "km-KM": "Khmer",
+    "kn-IN": "Kannada",
+    "ko-KR": "Korean",
+    "ku-TR": "Kurdish",
+    "ky-KG": "Kyrgyz",
+    "la-VA": "Latin",
+    "lo-LA": "Lao",
+    "lv-LV": "Latvian",
+    "men-SL": "Mende",
+    "mg-MG": "Malagasy",
+    "mi-NZ": "Maori",
+    "ms-MY": "Malay",
+    "mt-MT": "Maltese",
+    "my-MM": "Burmese",
+    "ne-NP": "Nepali",
+    "niu-NU": "Niuean",
+    "nl-NL": "Dutch",
+    "no-NO": "Norwegian",
+    "ny-MW": "Nyanja",
+    "ur-PK": "Pakistani",
+    "pau-PW": "Palauan",
+    "pa-IN": "Panjabi",
+    "ps-PK": "Pashto",
+    "pis-SB": "Pijin",
+    "pl-PL": "Polish",
+    "pt-PT": "Portuguese",
+    "rn-BI": "Kirundi",
+    "ro-RO": "Romanian",
+    "ru-RU": "Russian",
+    "sg-CF": "Sango",
+    "si-LK": "Sinhala",
+    "sk-SK": "Slovak",
+    "sm-WS": "Samoan",
+    "sn-ZW": "Shona",
+    "so-SO": "Somali",
+    "sq-AL": "Albanian",
+    "sr-RS": "Serbian",
+    "sv-SE": "Swedish",
+    "sw-SZ": "Swahili",
+    "ta-LK": "Tamil",
+    "te-IN": "Telugu",
+    "tet-TL": "Tetum",
+    "tg-TJ": "Tajik",
+    "th-TH": "Thai",
+    "ti-TI": "Tigrinya",
+    "tk-TM": "Turkmen",
+    "tl-PH": "Tagalog",
+    "tn-BW": "Tswana",
+    "to-TO": "Tongan",
+    "tr-TR": "Turkish",
+    "uk-UA": "Ukrainian",
+    "uz-UZ": "Uzbek",
+    "vi-VN": "Vietnamese",
+    "wo-SN": "Wolof",
+    "xh-ZA": "Xhosa",
+    "yi-YD": "Yiddish",
+    "zu-ZA": "Zulu"
+}
+
+
+
+
+
+
+let selectTag = document.querySelectorAll("select");
+
+selectLanguage();
+
+
+function selectLanguage() {
+
+    selectTag.forEach((tag, id) => {
+
+        for (let country_code in countries) {
+
+            let selected;
+
+            if (id == 0 && country_code == "en-GB") {
+                selected = "selected";
+            }
+
+            else if (id == 1 && country_code == "he-IL") {
+                selected = "selected";
+            }
+
+            let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+            tag.insertAdjacentHTML("beforeend", option);
+        }
+    });
+}
+
+
+
+
+
+
+
+
+let translateBtn = document.querySelector("#ClickTranslate")
+
+
+function TranslateTextFunction() {
+
+    let toText = document.querySelector(".to-text");
+    let fromText = document.querySelector(".from-text")
+
+
+    let text = fromText.value.trim(),
+        translateFrom = selectTag[0].value,
+        translateTo = selectTag[1].value;
+
+
+    if (!text) return;
+
+    toText.setAttribute("placeholder", "Translating...");
+
+    // use this url https://mymemory.translated.net/doc/spec.php
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+
+
+    fetch(apiUrl).then(res => res.json()).then(data => {
+
+        toText.value = data.responseData.translatedText;
+        data.matches.forEach(data => {
+            if (data.id === 0) {
+                toText.value = data.translation;
+            }
+        });
+
+        toText.setAttribute("placeholder", "Translation");
+    });
+
+}
+
+
+translateBtn.addEventListener("click", TranslateTextFunction)
